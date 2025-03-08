4d197f6e11c12e12c7e171403b8431aa500a489c
João Vitor Mesquita de Lima <joaomesquitalima6@gmail.com>
2025-03-08 19:02:27 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <section> 
+     <h1>O que você faz importa</h1>
+     <div>
+     <a>Criar conta</a>
+     <a>Entrar</a>
+     </div>
+     
+    </section>
+    
+      
+  );
+}
+
+export default App;
